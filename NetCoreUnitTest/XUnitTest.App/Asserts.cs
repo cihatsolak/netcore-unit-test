b74e712df255b3d533b6a93d63632eb671c4149d
Cihat Solak <cihatsolak@hotmail.com>
2021-05-08 19:09:35 +0300

    Contain / DoesNotContain

---
+++ NetCoreUnitTest/XUnitTest.App/Asserts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace XUnitTest.App
+{
+    /// <summary>
+    /// Asserts Metotlarını test etmek amacıyla oluşturulmuş örnek metotlar
+    /// </summary>
+    public class Asserts
+    {
+        public string GetBrand()
+        {
+            return "Volkswagen";
+        }
+
+        public List<string> GetModels()
+        {
+            return new List<string> { "Polo", "Golf", "Passat" };
+        }
+    }
+}
---
+++ NetCoreUnitTest/XUnitTest.Test/App/AssertsTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using XUnitTest.App;
+
+namespace XUnitTest.Test.App
+{
+    /// <summary>
+    /// Asserts Metotlarını test etmek 
+    /// </summary>
+    public class AssertsTest
+    {
+        /// <summary>
+        /// Contain / DoesNotContain
+        /// </summary>
+        [Fact]
+        public void ContainOrDoesNotContains()
+        {
+            var assert = new Asserts();
+
+            string brand = assert.GetBrand();
+            List<string> models = assert.GetModels();
+
+            Assert.Contains("agen", brand);
+            Assert.DoesNotContain("skoda", brand);
+
+            Assert.Contains(models, item => item.Equals("Ateca", StringComparison.OrdinalIgnoreCase));
+        }
+
+        /// <summary>
+        /// True / False
+        /// </summary>
+        [Fact]
+        public void TrueOrFalse()
+        {
+
+        }
+    }
+}
