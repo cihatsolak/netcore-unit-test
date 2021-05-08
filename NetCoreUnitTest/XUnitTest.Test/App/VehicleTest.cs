d707eaff0c9ca1fc1c32c6afaecb557089702479
Cihat Solak <cihatsolak@hotmail.com>
2021-05-08 18:51:53 +0300

    Assert.Equal

---
+++ NetCoreUnitTest/XUnitTest.Test/App/VehicleTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using XUnitTest.App;
+
+namespace XUnitTest.Test.App
+{
+    /// <summary>
+    /// Vehicle Test
+    /// </summary>
+    public class VehicleTest
+    {
+        [Fact]
+        public void CalculateConsumptionByDistanceTest()
+        {
+            //Arrage Evresi
+            int distance = 5;
+            int fuelPrice = 20;
+            var vehicle = new Vehicle();
+
+            //Act Evresi
+            int consumption = vehicle.CalculateConsumptionByDistance(distance, fuelPrice);
+
+            //Assert ->  Assert.Equal(beklenen değer, gerçek değer);
+            Assert.Equal(20, consumption);
+        }
+    }
+}
