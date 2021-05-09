a304a2eb52d4a41234aae0e6d658f53d4d032c5b
Cihat Solak <cihatsolak@hotmail.com>
2021-05-09 21:36:44 +0300

    ProductController added

--- NetCoreUnitTest/UnitTest.Web/Startup.cs
+++ NetCoreUnitTest/UnitTest.Web/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -7,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UnitTest.Web.Models;
 
 namespace UnitTest.Web
 {
@@ -22,6 +24,11 @@ namespace UnitTest.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UnitTestDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("Default"));
+            });
+
             services.AddControllersWithViews();
         }
 
