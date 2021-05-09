ad0002d09e6ced35340a22c9106abe5c546796fb
Cihat Solak <cihatsolak@hotmail.com>
2021-05-09 22:35:25 +0300

    Testing the index method

--- NetCoreUnitTest/UnitTest.Web.Test/Controllers/ProductControllerTest.cs
+++ NetCoreUnitTest/UnitTest.Web.Test/Controllers/ProductControllerTest.cs
@@ -1,17 +1,23 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System.Collections.Generic;
+using System.Linq;
 using UnitTest.Web.Controllers;
 using UnitTest.Web.Models;
 using UnitTest.Web.Repositories;
+using Xunit;
 
 namespace UnitTest.Web.Test.Controllers
 {
+    /// <summary>
+    /// ProductController Test Metotları
+    /// </summary>
     public class ProductControllerTest
     {
         private readonly Mock<IRepository<Product>> _productRepositoryMock;
         private readonly ProductsController _productController;
 
-        private List<Product> Products { get; set; }
+        private IEnumerable<Product> Products { get; set; }
 
         public ProductControllerTest()
         {
@@ -24,6 +30,33 @@ namespace UnitTest.Web.Test.Controllers
                 new Product(){Id = 2, Name = "Pen", Color="Blue",Price =200,Stock=23},
                 new Product(){Id = 3, Name = "Encyclopedia", Color="Green",Price =300,Stock=15},
             };
+        }
+
+        /// <summary>
+        /// IActionResult Index() ViewResult Testi
+        /// </summary>
+        [Fact]
+        public void Index_ActionExecutes_ReturnView()
+        {
+            IActionResult result = _productController.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        /// <summary>
+        /// IActionResult Index() Ürün Listesi Testi
+        /// </summary>
+        [Fact]
+        public void Index_ActionExecutes_ReturnProductList()
+        {
+            //Veri tabanından verileri çekmek yerine mock'lama işlemi yapıyorum.
+            _productRepositoryMock.Setup(p => p.GetAll()).Returns(Products);
+
+            IActionResult result = _productController.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result); //Geriye viewResult dönüyor mu?
+            var products = Assert.IsAssignableFrom<IEnumerable<Product>>(viewResult.Model); //Dönen model tipini kontrol ettim.
+            Assert.Equal<int>(3, products.Count()); //Dönen listenin eleman sayısını kontrol ettim.
         }
     }
 }
--- NetCoreUnitTest/UnitTest.Web/Controllers/ProductsController.cs
+++ NetCoreUnitTest/UnitTest.Web/Controllers/ProductsController.cs
@@ -133,7 +133,7 @@ namespace UnitTest.Web.Controllers
         // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public IActionResult DeleteConfirmed(int id)
         {
             var product = _productRepository.GetById((int)id);
             _productRepository.Delete(product);
