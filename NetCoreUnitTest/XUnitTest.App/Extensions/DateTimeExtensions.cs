ed642fc207b04bfd9a537104303f04e101332c5b
cihatsolak <cihatsolak@hotmail.com>
2022-07-16 17:10:03 +0300

    Add date time extensions

---
+++ NetCoreUnitTest/XUnitTest.App/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace XUnitTest.App.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToPrettyDate(this DateTime date, CultureInfo culture)
+        {
+            if (culture == null)
+            {
+                throw new ArgumentNullException(nameof(culture));
+            }
+
+            return date.ToString("dd MMMMM yyyy", culture);
+        }
+    }
+}
