0c310c0252300cbc0b9f071625fc3e4221a37668
cihatsolak <cihatsolak@hotmail.com>
2022-07-16 17:17:28 +0300

    Date time extensions test

---
+++ NetCoreUnitTest/XUnitTest.Test/Extensions/DateTimeExtensionsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+using XUnitTest.App.Extensions;
+
+namespace XUnitTest.Test.Extensions
+{
+    public class DateTimeExtensionsTest
+    {
+        [Fact]
+        public void ToPrettyDate_ShouldArgumentNullException_WhenCultureIsNull()
+        {
+            var result = Record.Exception(() => DateTime.Now.ToPrettyDate(null));
+            Assert.NotNull(result);
+            var exception = Assert.IsType<ArgumentNullException>(result);
+            var actual = exception.ParamName;
+            const string expected = "culture";
+            Assert.Equal(expected, actual);
+        }
+    }
+}
